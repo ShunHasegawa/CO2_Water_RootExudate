4ddff8bb82dd59ef12d54dfe8f1ba5ece5b77e14
Shun Hasegawa <ShunHasegawa@users.noreply.github.com>
2023-01-10 18:50:44 +0100

    VIF has been added

--- R/analysis_MultipleRegression.R
+++ R/analysis_MultipleRegression.R
@@ -22,6 +22,7 @@ summary(euc_OAr_env1)
 # no block effect
 euc_OAr_env2 <- lm(OArhizo_plant^(1/3) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z, euc_env_dd)
 # model diagnosis
+vif(euc_OAr_env2)
 qqresidPlot(euc_OAr_env2)
 # coefficients
 summary(euc_OAr_env2)
@@ -31,6 +32,7 @@ confint(euc_OAr_env2)
 # . Hakea ----------------------------------------------------------------
 hak_OAr_env1 <- lmer(OArhizo_plant^(-.4) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z + ClRootNo_root_Z +  (1|block), hak_env_dd)
 # model diagnosis
+vif(hak_OAr_env1)
 qqresidPlot(hak_OAr_env1)
 # coefficients
 summary(hak_OAr_env1)
@@ -40,6 +42,7 @@ confint(hak_OAr_env1)
 # . Microlaena -----------------------------------------------------------
 mic_OAr_env1 <- lmer(OArhizo_plant^(1/3) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z + (1|block), mic_env_dd)
 # model diagnosis
+vif(mic_OAr_env1)
 qqresidPlot(mic_OAr_env1)
 # coefficients
 summary(mic_OAr_env1)
@@ -54,6 +57,7 @@ summary(euc_OAe_env1)
 # no block effect
 euc_OAe_env2 <- lm(log(OAexuded_plant + .1) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z, euc_env_dd)
 # model diagnosis
+vif(euc_OAe_env2)
 qqresidPlot(euc_OAe_env2)
 # coefficients
 summary(euc_OAe_env2)
@@ -66,6 +70,7 @@ summary(hak_OAe_env1)
 # no block effect
 hak_OAe_env2 <- lm(log(OAexuded_plant) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z + ClRootNo_root_Z, hak_env_dd)
 # model diagnosis
+vif(hak_OAe_env2)
 qqresidPlot(hak_OAe_env2)
 # coefficients
 summary(hak_OAe_env2)
@@ -75,6 +80,7 @@ confint(hak_OAe_env2)
 # . Microlaena -----------------------------------------------------------
 mic_OAe_env1 <- lmer(OAexuded_plant^(1/3) ~ total_biomass_Z + RSR_logZ + soil_nitrate_Z + soil_ammonium_logZ + soil_phosphate_Z + (1|block), mic_env_dd)
 # model diagnosis
+vif(mic_OAe_env1)
 qqresidPlot(mic_OAe_env1)
 # coefficients
 summary(mic_OAe_env1)
